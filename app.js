7dce0d11f2944a494d624b61cd4811458f46d0ce
aagr29 <63660748+aagr29@users.noreply.github.com>
2020-05-16 15:14:23 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,271 @@
+const express=require('express')
+const app = express()
+const bodyParser=require('body-parser')
+const bcrypt= require('bcrypt')
+const pgp = require('pg-promise')()
+const session = require('express-session') // for cookies we are using express-session
+const path=require('path')
+
+const PORT = 3000 // local port
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/views')); // for css files and media files to be attached to ejs page
+
+const CONNECTION_STRING = "postgres://postgres:9525@localhost:5432/aniket_it2"
+// connection string to connect to database
+
+
+const SALT_ROUND=10 //  for bycrpt , number of salt rounds defines encrytion layers
+
+
+app.use(session({
+  secret: 'aniket29894',//can be anything
+  resave : false,
+  saveUninitialized: false
+}))//cookie for session 
+
+//body-parser
+app.use(bodyParser.urlencoded({extended: false}))
+
+//checking db
+const db = pgp(CONNECTION_STRING)
+console.log(db)//if database is connected display syntaxs in terminal
+
+//listening on port 3000
+app.listen(PORT,() => {
+    console.log(`Server has started on ${PORT}`)
+  })
+
+
+//display index page  
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+
+
+//display registration page
+app.get('/registration', (req, res) =>{
+    res.render('registration',{message:NaN});
+  });//if message exists then ejs displays a message
+
+//display login page
+app.get('/login', (req, res) =>{
+    res.render('login',{message:NaN});
+  });//if message exists then ejs displays a message
+
+
+  // display weight page
+  app.get('/weight', (req, res) =>{
+    res.render('weight');
+  });
+
+  // display mood page
+  app.get('/mood', (req, res) =>{
+    res.render('mood');
+  });
+
+
+// dummy page just to display session value
+  app.get('/display', (req, res) =>{
+    var val = req.session.user;
+    res.render('display',{user:val});
+});//displays sesssion values of logged in user
+
+
+
+app.get('/home', (req, res) =>{
+  res.render('home');
+});
+// log out get request
+
+
+
+// log out get request (completed by destroying session and taking to home page)
+app.get('/logout',(req,res,next)=>{
+  if(req.session){
+    req.session.destroy((error)=>{
+      if(error){
+        next(error)
+      }else{
+        res.redirect('/')
+      }
+    })
+  }
+})
+
+
+// post for registration page
+app.post('/registration',(req,res) => {
+
+    //vaules in form
+    let username = req.body.username
+    let password = req.body.password
+    let height= req.body.height
+    let age= req.body.age
+    let gender= req.body.gender
+
+
+    db.oneOrNone('SELECT userid FROM users WHERE username = $1',[username])//checking usename exits
+    .then((user) => {
+      console.log(user)
+      if(user) {
+        res.render('registration',{message:"Username already exsist"});//if yes display message
+      } else {
+        // insert user into the users table
+       bcrypt.hash(password,SALT_ROUND,function(error,hash){
+         if(error == null){
+          db.none('INSERT INTO users(username,password,age,height,gender) VALUES($1,$2,$3,$4,$5)',[username,hash,age,height,gender])
+          .then(() => {
+            res.redirect('/')
+          })
+         }
+       })
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  })
+
+  app.post('/login',(req,res)=>{
+  
+    let username = req.body.username
+    let password = req.body.password
+  
+    db.oneOrNone('SELECT userid,username,password,age,height,gender FROM users WHERE username = $1',[username])
+    .then((user) => {
+      if(user){//check for users password
+  
+        bcrypt.compare(password,user.password,function(error,result){
+        if(result){
+  
+          if(req.session){
+            req.session.user={userId:user.userid, username:user.username, age:user.age ,height:user.height,gender:user.gender}
+          }
+          res.redirect('/weight')
+        
+        }else{
+          res.render('login',{message:'invalid username or password'})
+        }
+      })
+  
+    }else{
+      res.render('login',{message:'invalid username or password'})
+    }
+  }).catch(error => {
+    console.log(error);
+  })
+  })
+
+
+ //mood post api
+
+ app.post('/set_mood', function(req, res){
+  let mood_score=req.body.mood_score
+  let userId=req.session.user.userId
+  console.log(mood_score);
+
+  db.none('INSERT INTO mood (mood_score,userid) VALUES($1,$2)',[mood_score,userId])
+  .then(()=>{
+    res.render("mood")//takes to mood page
+
+  }).catch(error => {
+    console.log(error);
+  })
+})
+
+// get weight and height for all days for user
+app.get('/get_bmi', function(req, res) {
+  let userId=req.session.user.userId
+
+  db.any('SELECT userid,AVG(height) AS AVG_HGT,AVG(weight) AS AVG_WGT,date(date_time) FROM weight_records WHERE userid = $1 GROUP BY date(date_time), userid',[userId])
+    .then((result) => {
+      let heights = [];
+      let weights = [];
+      let dates = [];
+
+      let data = result;
+
+      for(i=0; i< data.length; i++) {
+        obj = data[i];
+        heights.push(Math.round(parseFloat(obj["avg_hgt"]), 2));
+        weights.push(Math.round(parseFloat(obj["avg_wgt"]), 2));
+        dates.push(obj["date"]);
+      }
+
+      res.status(200).json({'heights': heights, "weights": weights, "dates": dates})
+  }).catch(error => {
+    console.log(error);
+  })
+})
+//Send latest weight and height  of user to calculate bmi
+app.get('/get_latest_bmi', function(req, res) {
+  let userId=req.session.user.userId
+
+  db.any('SELECT userid,height,weight,date_time FROM weight_records WHERE userid = $1 ORDER BY date_time DESC LIMIT 1',[userId])
+    .then((result) => {
+      res.status(200).json({'result': result[0]})
+  }).catch(error => {
+    console.log(error);
+  })
+})
+
+app.get('/bmi', (req, res) =>{
+  res.render('bmi',{message:NaN});
+});//if message exists then ejs displays a message
+
+
+
+  //weight post
+
+  app.post('/weight', function(req, res){
+    let weight=req.body.weight
+    let age=req.session.user.age
+    let height=req.session.user.height
+    let gender=req.session.user.gender
+    let userId=req.session.user.userId
+    
+  
+    db.none('INSERT INTO weight_records (weight,age,height,gender,userid) VALUES($1,$2,$3,$4,$5)',[weight,age,height,gender,userId])
+    .then(()=>{
+    res.render("mood")//takes to mood page
+    }).catch(error => {
+      console.log(error);
+    })
+  })
+
+  
+  //game code starts here
+
+
+  app.get('/game', function(req, res){
+     res.render('game');
+});
+
+app.post('/set_game_score', function(req, res){
+  let game_score=req.body.game_score
+  let userId=req.session.user.userId
+  console.log(game_score);
+
+  db.none('INSERT INTO game_records (game_score,userid) VALUES($1,$2)',[game_score,userId])
+  .then(()=>{
+    res.render("home")//takes to home pagenode
+
+  }).catch(error => {
+    console.log(error);
+  })
+})
+
+
+
+
+
+
+
+
+
+
+  
+  
---
+++ views/game.js
@@ -0,0 +1,236 @@
+
+const startButton= document.getElementById('start-btn')
+const endloop = document.getElementById('answer-buttons')
+const nextButton= document.getElementById('next-btn')
+const endofgamebutton = document.getElementById('endgame')
+
+const questionContainerElements= document.getElementById('question_container')
+const questionElement = document.getElementById('question')
+const answerButtonsElment = document.getElementById('answer-buttons')
+var score = 0
+
+// set the question randomly
+
+var shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', ()=>{
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+function startGame(){
+startButton.classList.add('hide');
+shuffledQuestions = questions.sort(()=> Math.random() - .5)
+shuffledQuestions = shuffledQuestions.slice(0,8)
+console.log(shuffledQuestions.length);
+
+currentQuestionIndex = 0
+questionContainerElements.classList.remove('hide');
+
+setNextQuestion();
+
+
+}
+
+function setNextQuestion(){
+    resetState()
+showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question){
+questionElement.innerText = question.question
+question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct){
+        button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElment.appendChild(button)
+})
+}
+        
+
+function resetState(){
+clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElment.firstChild){
+        answerButtonsElment.removeChild
+        (answerButtonsElment.firstChild)
+    }
+}
+
+function selectAnswer(e){
+const selectedButton = e.target
+const correct = selectedButton.dataset.correct
+setStatusClass(document.body, correct)
+Array.from(answerButtonsElment.children).forEach(button=>{
+    setStatusClass(button, button.dataset.correct)
+})
+if(shuffledQuestions.length > currentQuestionIndex +1){
+
+    nextButton.classList.remove('hide')
+}else{
+    endloopMethod();
+}
+}
+
+function endloopMethod(){
+    endloop.classList.add('hide')
+    document.getElementById('question').innerHTML="Congragulations! Game Finished"
+    endofgamebutton.classList.remove('hide')
+    process.exit(1)
+}
+
+function setStatusClass(element, correct){
+
+    clearStatusClass(element)
+    if (correct){
+        element.classList.add('correct')
+        console.log("correct");
+        score  = score + 1;
+        console.log(score);  
+    }
+    else{
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element){
+element.classList.remove('correct')
+element.classList.remove('wrong')
+
+}
+
+$(document).ready(function(){
+    $(document).on("click", "#endgame", function() {
+        $.post("/set_game_score", {game_score: parseInt(score)}, function(){
+            window.location = "/home";
+        });
+    });
+});
+
+const questions = [
+    {QID: "1",
+        question: "I use (below) to brush teeth",
+      answers: [
+          {text: 'Tooth Brush', correct: true},
+          {text: 'Glasses', correct: false},
+          {text: 'Hair Brush', correct: false},
+          {text: 'tissue', correct: false},]
+    },
+    {QID: "2",
+        question: "Apple is a",
+    answers: [
+        {text: 'car', correct: false},
+        {text: 'fruit', correct: true},
+        {text: 'bed', correct: false},
+        {text: 'toaster', correct: false},]
+    },
+    {QID: "3",
+        question: "Kitchen is where I can",
+    answers: [
+      {text: 'withdraw money', correct: false},
+      {text: 'take a shower', correct: false},
+      {text: 'wash my car', correct: false},
+      {text: 'cook dinner', correct: true},]
+    },
+    {QID: "4",
+    question: "Animal name beginning with the letter S",
+    answers: [
+    {text: 'Tigar', correct: false},
+    {text: 'Sugar', correct: false},
+    {text: 'Spider', correct: false},
+    {text: 'Skunk', correct: true},]
+    },
+    {QID: "5", 
+    question: "40 * 50 = ",
+    answers: [
+    {text: '40', correct: false},
+    {text: '50', correct: false},
+    {text: '300', correct: false},
+    {text: '2000', correct: true},]
+    },
+    {QID: "6",
+    question: "Which one is the largest number?",
+    answers: [
+    {text: '456', correct: true},
+    {text: '123', correct: false},
+    {text: '111', correct: false},
+    {text: '340', correct: false},]
+    },
+    {QID: "7",
+    question: "what the missing number at the end of the series. 5, 12, 19, 26,?",
+    answers: [
+    {text: '31', correct: false},
+    {text: '32', correct: false},
+    {text: '33', correct: false},
+    {text: '34', correct: true},]
+    },
+    {QID: "8",
+    question: "5 + 2 = ",
+    answers: [
+    {text: '5', correct: false},
+    {text: '7', correct: true},
+    {text: '52', correct: false},
+    {text: '12', correct: false},]
+    },
+    {QID: "9",
+    question: "I go to a bank to",
+    answers: [
+    {text: 'Deposit money', correct: true},
+    {text: 'Watch a movie', correct: false},
+    {text: 'Cook dinner', correct: false},
+    {text: 'cut my hair', correct: false},]
+    }, 
+    {QID: "10",
+    question: "33 / 11 =",
+    answers: [
+    {text: '33', correct: false},
+    {text: '3', correct: true},
+    {text: '11', correct: false},
+    {text: 'unknown', correct: false},]
+    },
+    {QID: "11",
+    question: "Fill in the missing word: An Espress Book Machine (EBM) prints and binds a high-quality copy of a book right in the store, in just a few minutes. There’s a catalogue of millions of books to choose from, including rare manuscripts and books that publishers are no longer _______",
+    answers: [
+    {text: 'printing', correct: true},
+    {text: 'asking', correct: false},
+    {text: 'seeking', correct: false},
+    {text: 'recommending', correct: false},]
+    },
+    {QID: "12",
+    question: "Which of the following is the opposite of the word dark?",
+    answers: [
+    {text: 'Gloomy', correct: false},
+    {text: 'Happy', correct: false},
+    {text: 'Day', correct: false},
+    {text: 'Light', correct: true},]
+    },
+    {QID: "13",
+    question: "which number is the smallest?",
+    answers: [
+    {text: '67', correct: false},
+    {text: '90', correct: false},
+    {text: '16.5', correct: true},
+    {text: '34', correct: false},]
+    },
+    {QID: "14",
+    question: "Country name beginning with the letter A",
+    answers: [
+    {text: 'Adelaide', correct: false},
+    {text: 'Australia', correct: true},
+    {text: 'India', correct: false},
+    {text: 'China', correct: false},]
+    },
+    {QID: "15",
+    question: "Letter is to word as house is to",
+    answers: [
+    {text: 'mansion', correct: false},
+    {text: 'room', correct: true},
+    {text: 'hospital', correct: false},
+    {text: 'homeless', correct: false},]
+    },
+  ];
